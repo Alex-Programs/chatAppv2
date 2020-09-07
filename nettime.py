14ad589ebe2a0ca97e57a4a7c910f20cd764bf68
AlexAndHisScripts <40502404+AlexAndHisScripts@users.noreply.github.com>
2020-09-07 19:07:52 +0100

    Added test code

--- nettime.py
+++ nettime.py
@@ -2,19 +2,21 @@ import requests
 import time
 
 class globals:
-    timeOverride = False
+    timeOverride =  True
     lastOverrideTime = 0
     lastOverrideOffset = 0
     baseUrl = "http://86.31.173.35:443"
 
 def get_time():
-    if time.time() > globals.lastOverrideTime + 30:
+    if time.time()+8000 > globals.lastOverrideTime + 10:
         realtime = float(requests.get(globals.baseUrl + "/get_time").content)
-        globals.lastOverrideOffset = realtime - time.time()
-        globals.lastOverrideTime = time.time()
+        globals.lastOverrideOffset = realtime - (time.time()+8000)
+        globals.lastOverrideTime = time.time()+8000
 
         print("Time: " + str(realtime))
+        print("Offset: " + str(globals.lastOverrideOffset))
         return realtime
         
     else:
-        return time.time() + globals.lastOverrideOffset
+        print("Offset: " + str(globals.lastOverrideOffset))
+        return (time.time()+8000) + globals.lastOverrideOffset
